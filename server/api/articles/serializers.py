faac324da25c71c8808e7bd3c0d739e7ee129a33
AdamPekny <peknyadam9@gmail.com>
2022-10-11 17:02:48 +0200

    Paragraph serializers added.

--- server/api/articles/serializers.py
+++ server/api/articles/serializers.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 from .models import Articles, Paragraph
 
 
+class ParagraphSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paragraph
+        fields = [
+            'text',
+            'article',
+        ]
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     thumbnail_path = serializers.SerializerMethodField()
     endpoint = serializers.SerializerMethodField()
@@ -31,4 +40,4 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def get_body(obj):
-        return Paragraph.objects.filter(article=obj.id)
+        return ParagraphSerializer(Paragraph.objects.filter(article=obj.id), many=True).data
